e3eded81f18f6034e07cef409584e1bf409ccb4d
Jaydan Urwin <jaydan@treefarm.studio>
2024-11-07 10:54:18 -0700

    bun hono example

---
+++ bun-hono/src/index.ts
@@ -0,0 +1,20 @@
+import { Hono } from '@hono/hono'
+import { html, Layout } from '@sapling/sapling'
+
+const app = new Hono()
+
+app.get('/', async (c) => {
+  const time = new Date().toLocaleTimeString()
+  const page = await Layout({
+    children: html`
+      <div class="flex flex-col justify-center items-center h-screen gap-4">
+        <h1 class="text-6xl font-bold">Hello World 🌍</h1>
+        <p class="text-2xl">This is a site using <a class="text-blue-500" href="https://sapling.build">Sapling</a>, <a class="text-blue-500" href="https://hono.dev">Hono</a>, and <a class="text-blue-500" href="https://bun.sh">Bun</a></p>
+        <p class="text-base font-mono text-gray-500">It was server rendered at ${time}</p>
+      </div>
+    `,
+  })
+  return c.html(page)
+})
+
+export default app
