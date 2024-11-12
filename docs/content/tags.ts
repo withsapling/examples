7acdbcfac6fa223fc0f2c7ffc7a867d0968cc8d9
Jaydan Urwin <jaydan@treefarm.studio>
2024-11-12 13:22:53 -0700

    setup docs

---
+++ docs/content/tags.ts
@@ -0,0 +1,19 @@
+export type Tag = {
+  name: string;
+  slug: string;
+}
+
+export const tags: Tag[] = [
+  {
+    name: "Sapling",
+    slug: "sapling",
+  },
+  {
+    name: "JavaScript",
+    slug: "javascript",
+  },
+  {
+    name: "TypeScript",
+    slug: "typescript",
+  },
+];
---
+++ docs/utils/helpers.ts
@@ -0,0 +1,3 @@
+export function getAllBlogPosts() {
+  return [];
+}
