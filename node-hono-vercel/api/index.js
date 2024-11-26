675b0baf2f4b0ba6eac031b86d65712b880f7075
Jaydan Urwin <jaydan@treefarm.studio>
2024-11-26 11:02:44 -0700

    vercel example

---
+++ node-hono-vercel/api/index.js
@@ -0,0 +1,43 @@
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
+import { html, Layout } from "@sapling/sapling";
+
+const app = new Hono();
+
+app.get("/", async (c) => {
+  const time = new Date().toLocaleTimeString();
+  const page = await Layout({
+    head: html`<title>Hello World 🌍</title>`,
+    children: html`
+      <div
+        class="px-12 flex flex-col justify-center items-center h-screen gap-4"
+      >
+        <h1 class="text-6xl font-bold">Hello World 🌍</h1>
+        <p class="text-2xl">
+          This is a site using
+          <a class="text-blue-500 hover:underline" href="https://sapling.build"
+            >Sapling</a
+          >,
+          <a class="text-blue-500 hover:underline" href="https://hono.dev"
+            >Hono</a
+          >, and
+          <a class="text-blue-500 hover:underline" href="https://nodejs.org"
+            >Node.js</a
+          >
+        </p>
+        <p class="text-base font-mono text-gray-500">
+          It was server rendered at ${time} deployed on Vercel
+        </p>
+      </div>
+    `,
+  });
+  return c.html(page);
+});
+
+const handler = handle(app);
+
+export const GET = handler;
+export const POST = handler;
+export const PATCH = handler;
+export const PUT = handler;
+export const OPTIONS = handler;
