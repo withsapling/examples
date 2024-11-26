e51db3e5ba44483b1da4346c262c2c490994cbfd
Jaydan Urwin <jaydan@treefarm.studio>
2024-11-26 14:46:50 -0700

    env

--- basics/index.ts
+++ basics/index.ts
@@ -6,14 +6,15 @@ const router = new Router();
 
 // Home page
 router.get("/", async (c) => c.html(await Home()));
-
-// Enter additional routes below
-
+// Enter additional routes here
 
 // Serve static files
 // The location of this is important. It should be the last route you define.
 router.get("/*", serveStatic({
   directory: "./static",
+  // this will disable caching for static files in development
+  // it is automatically passed in when you run deno task dev
+  dev: Deno.env.get("ENV") === "development",
 }));
 
 // 404 Handler
