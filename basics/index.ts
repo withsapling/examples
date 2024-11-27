e66bf70255965c88dcfc9dec4b944a3b10de715e
Jaydan Urwin <jaydan@treefarm.studio>
2024-11-26 23:23:12 -0700

    sapling upgrade

--- basics/index.ts
+++ basics/index.ts
@@ -1,16 +1,16 @@
-import { Router, serveStatic } from "@sapling/router";
+import { Sapling, serveStatic } from '@sapling/sapling'
 import NotFoundLayout from "./layouts/NotFoundLayout.ts";
 import { Home } from "./pages/Home.ts";
 
-const router = new Router();
+const site = new Sapling();
 
 // Home page
-router.get("/", async (c) => c.html(await Home()));
+site.get("/", async (c) => c.html(await Home()));
 // Enter additional routes here
 
 // Serve static files
 // The location of this is important. It should be the last route you define.
-router.get("/*", serveStatic({
+site.get("/*", serveStatic({
   directory: "./static",
   // this will disable caching for static files in development
   // it is automatically passed in when you run deno task dev
@@ -18,7 +18,7 @@ router.get("/*", serveStatic({
 }));
 
 // 404 Handler
-router.setNotFoundHandler(async (c) => c.html(await NotFoundLayout()));
+site.setNotFoundHandler(async (c) => c.html(await NotFoundLayout()));
 
 Deno.serve({
   port: 8080,
@@ -27,5 +27,5 @@ Deno.serve({
       `\nSapling Server is running on %chttp://localhost:8080\n`,
       "color: green; font-weight: bold"
     ),
-  handler: router.fetch,
+  handler: site.fetch,
 });
