86158ec6470737f58a65f473164a0ab705e3a456
Jaydan Urwin <jaydan@treefarm.studio>
2024-12-02 13:29:13 -0700

    redirects and sapling page

--- deno/with-react-spa/index.ts
+++ deno/with-react-spa/index.ts
@@ -11,8 +11,31 @@ site.get("/", async (c) => {
           class="px-12 flex flex-col justify-center items-center h-screen gap-4"
         >
           <h1 class="text-6xl font-bold">Sapling & React SPA</h1>
-          <a class="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600" href="/app">
-            Open App
+          <div class="flex gap-4">
+            <a class="border border-gray-300 text-gray-800 px-4 py-2 rounded-md hover:bg-gray-100" href="/page">
+              Sapling Page
+            </a>
+            <a class="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600" href="/app">
+              Open App
+            </a>
+          </div>
+        </div>
+      `,
+    })
+  );
+});
+
+site.get("/page", async (c) => {
+  return c.html(
+    await Layout({
+      head: html` <title>Sapling Page</title>`,
+      children: html`
+        <div
+          class="px-12 flex flex-col justify-center items-center h-screen gap-4"
+        >
+          <h1 class="text-6xl font-bold">Sapling Page</h1>
+          <a class="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600" href="/">
+            Back to Home
           </a>
         </div>
       `,
@@ -20,12 +43,23 @@ site.get("/", async (c) => {
   );
 });
 
+
+
 site.get("/*", serveStatic({
   directory: "./static",
   dev: true,
 }));
 
+
 site.setNotFoundHandler((c) => {
+  // if the path starts with /app and is a 404, redirect to /app
+  const url = new URL(c.req.url);
+  const pathname = url.pathname;
+
+  if (pathname.startsWith("/app")) {
+    return c.redirect("/app");
+  }
+  // otherwise, redirect to home
   return c.redirect("/");
 });
 
