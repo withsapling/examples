564f0ecdb4cc7192af39f302e54a725630b0ca06
Jaydan Urwin <jaydan@treefarm.studio>
2024-12-03 10:53:50 -0700

    cleanup

--- deno/with-sanity/sapling/README.md
+++ deno/with-sanity/sapling/README.md
@@ -1,8 +1,2 @@
-# Sapling Basics Template
-
-## Usage
-
-```sh
-deno task start
-```
+Please read the [README.md](../README.md) file in the root of the repo for instructions on how to run this template.
 
--- deno/with-sanity/sapling/lib/portableText.ts
+++ deno/with-sanity/sapling/lib/portableText.ts
@@ -3,6 +3,5 @@ import { toHTML } from '@portabletext/to-html'
 // convert Portable Text to HTML
 export async function portableTextToHTML(portableTextBlocks: any) {
   const html = await toHTML(portableTextBlocks, {})
-  console.log(html)
   return html
 }
