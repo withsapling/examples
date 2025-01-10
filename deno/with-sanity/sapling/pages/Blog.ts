8058b9de31dd6af0446a0bdd0f90dd58d5bd85ce
Jaydan Urwin <jaydan@treefarm.studio>
2025-01-10 11:49:06 -0700

    sanity

--- deno/with-sanity/sapling/pages/Blog.ts
+++ deno/with-sanity/sapling/pages/Blog.ts
@@ -9,7 +9,7 @@ export async function Blog() {
     title: "Sapling & Sanity Blog",
     children: await html`
     <main class="max-w-screen-lg min-h-screen mx-auto px-4 py-16 flex flex-col items-center justify-center font-sans gap-4">
-    <h1 class="text-4xl font-bold">Sapling & Contentful Blog</h1>
+    <h1 class="text-4xl font-bold">Sapling & Sanity Blog</h1>
     <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
       ${posts.map((post: any) => BlogPostCard({ post }))}
     </div>
