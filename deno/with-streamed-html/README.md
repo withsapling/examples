21d7347c511453977a03eb6f814746e699c0bebb
Jaydan Urwin <jaydan@treefarm.studio>
2025-01-12 12:45:14 -0700

    streaming html example

---
+++ deno/with-streamed-html/README.md
@@ -0,0 +1,51 @@
+# Sapling with Streamed HTML Response
+
+Sapling's Layout component has a built-in `stream` option that allows you to stream the HTML response. This is useful for large pages that could benefit from a faster time to first byte.
+
+If you are using Sapling's Router it is as simple as adding `stream: true` to the Layout component.
+
+However, if you're using Hono or another API framework, you'll need to handle the streaming manually.
+
+
+## Sapling Router Example
+
+```ts
+import { Sapling, Layout, html, type Context } from "@sapling/sapling";
+
+const site = new Sapling();
+
+site.get("/", async (c: Context) => {
+  return c.html(
+    await Layout({
+      stream: true,
+      children: html`<div>Hello World</div>`,
+    })
+  );
+});
+```
+
+## Hono Example
+
+```ts
+import { Hono, type Context } from "@hono/hono";
+import { Layout, html } from "@sapling/sapling";
+
+const app = new Hono();
+
+app.get("/", async (c: Context) => {
+  const stream = await Layout({
+    stream: true,
+    children: html`<div>Hello World</div>`,
+  });
+  return new Response(stream, {
+    headers: { "content-type": "text/html; charset=utf-8" },
+  });
+});
+```
+
+## Usage
+
+```
+deno task dev
+```
+
---
+++ deno/with-streamed-html/index.ts
@@ -0,0 +1,95 @@
+import { Sapling, Layout, html, type Context } from "@sapling/sapling";
+
+const site = new Sapling();
+
+site.get("/", async (c: Context) => {
+  const time = new Date().toLocaleTimeString();
+  return c.html(
+    await Layout({
+      stream: true,
+      head: html` <title>Hello World 🌍</title>`,
+      children: html`
+        <div
+          class="px-12 flex flex-col justify-center items-center h-screen gap-4"
+        >
+          <h1 class="text-6xl font-bold">Hello World 🌍</h1>
+          <p class="text-2xl">
+            This is a site using
+            <a
+              class="text-blue-500 hover:underline"
+              href="https://sapling.land"
+              >Sapling</a
+            >
+            and
+            <a class="text-blue-500 hover:underline" href="https://deno.com"
+              >Deno</a
+            >
+          </p>
+          <p class="text-base font-mono text-gray-500">
+            It was server rendered at ${time}
+          </p>
+          <form
+            action="/"
+            method="get"
+            onsubmit="this.action=this.action+this.name.value"
+            class="flex gap-2"
+          >
+            <input
+              class="border border-gray-300 rounded-md px-2 py-1"
+              placeholder="Enter a name"
+              type="text"
+              name="name"
+            />
+            <button
+              class="bg-blue-500 text-white px-4 py-2 rounded-md"
+              type="submit"
+            >
+              Say Hello
+            </button>
+          </form>
+        </div>
+      `,
+    })
+  );
+});
+
+site.get("/:name", async (c: Context) => {
+  const name = c.req.param("name");
+  const capitalizedName = name.charAt(0).toUpperCase() + name.slice(1);
+  const time = new Date().toLocaleTimeString();
+  return c.html(
+    await Layout({
+      stream: true,
+      head: html`<title>Hello ${capitalizedName} 🌍</title>`,
+      children: html`
+        <div
+          class="px-12 flex flex-col justify-center items-center h-screen gap-4"
+        >
+          <h1 class="text-6xl font-bold">Hello ${capitalizedName} 🌍</h1>
+          <p class="text-2xl">
+            This is a site using
+            <a
+              class="text-blue-500 hover:underline"
+              href="https://sapling.land"
+              >Sapling</a
+            >
+            and
+            <a class="text-blue-500 hover:underline" href="https://deno.com"
+              >Deno</a
+            >
+          </p>
+          <p class="text-base font-mono text-gray-500">
+            It was server rendered at ${time}
+          </p>
+          <a class="text-blue-500 hover:underline" href="/">Go back</a>
+        </div>
+      `,
+    })
+  );
+});
+
+Deno.serve({
+  port: 8080,
+  onListen: () => console.log("Server is running on http://localhost:8080"),
+  handler: site.fetch,
+});
