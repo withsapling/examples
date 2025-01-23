b15e01bf59134d1001789e58bffcbc766e4808e6
Jaydan Urwin <jaydan@treefarm.studio>
2025-01-23 14:20:50 -0700

    update example

--- node/basics/src/index.ts
+++ node/basics/src/index.ts
@@ -1,12 +1,15 @@
+import dotenv from "dotenv";
 import { serve } from "@hono/node-server";
 import { Sapling, serveStatic, type Context } from "@sapling/sapling";
 import { Home } from "./pages/Home.js";
 import NotFoundLayout from "./layouts/NotFoundLayout.js";
 
+dotenv.config();
+
 const site = new Sapling({
   // this will disable caching for static files in development
   // it is automatically passed in when you run deno task dev
-  dev: true,
+  dev: process.env.ENV === "development",
 });
 
 // Home page
@@ -15,10 +18,12 @@ site.get("/", async (c: Context) => c.html(await Home()));
 
 // Serve static files
 // The location of this is important. It should be the last route you define.
-site.get("/*", serveStatic({
-  directory: "./static",
-
-}));
+site.get(
+  "/*",
+  serveStatic({
+    directory: "./static",
+  })
+);
 
 // Set the not found handler last to catch all 404s
 site.setNotFoundHandler(async (c: Context) => c.html(await NotFoundLayout()));
