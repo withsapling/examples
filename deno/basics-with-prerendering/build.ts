e21d2a4b55921220253f11d2045041d0116c3224
Jaydan Urwin <jaydan@treefarm.studio>
2025-01-24 12:50:10 -0700

    build comments

--- deno/basics-with-prerendering/build.ts
+++ deno/basics-with-prerendering/build.ts
@@ -1,4 +1,7 @@
 import { site } from "./index.ts";
 
+// this is the build script to prerender your .prerender() routes
+// run deno task build to prerender your pages into the ./dist directory
+// you can obviously add other build optimizations here as needed
 await site.generatePrerenderedPages("./dist");
 Deno.exit(0);
