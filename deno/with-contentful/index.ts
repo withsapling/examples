8024acf9c7f52f8dbef32f2698c7ca512e3a88cc
Jaydan Urwin <jaydan@treefarm.studio>
2025-01-31 13:29:30 -0700

    contentful

--- deno/with-contentful/index.ts
+++ deno/with-contentful/index.ts
@@ -1,4 +1,4 @@
-import { Sapling, serveStatic } from '@sapling/sapling'
+import { Sapling, serveStatic } from "@sapling/sapling";
 import NotFoundLayout from "./layouts/NotFoundLayout.ts";
 import { Blog } from "./pages/Blog.ts";
 import { BlogPost } from "./pages/BlogPost.ts";
@@ -6,21 +6,26 @@ const site = new Sapling();
 
 // Home page
 site.get("/", async (c) => c.html(await Blog()));
-site.get("/blog/:slug", async (c) => c.html(await BlogPost({ params: { slug: c.req.param("slug") } })));
+site.get("/blog/:slug", async (c) =>
+  c.html(await BlogPost({ params: { slug: c.req.param("slug") } }))
+);
 
 // Enter additional routes here
 
 // Serve static files
 // The location of this is important. It should be the last route you define.
-site.get("/*", serveStatic({
-  directory: "./static",
-  // this will disable caching for static files in development
-  // it is automatically passed in when you run deno task dev
-  dev: Deno.env.get("ENV") === "development",
-}));
+site.get(
+  "/*",
+  serveStatic({
+    root: "./static",
+    // this will disable caching for static files in development
+    // it is automatically passed in when you run deno task dev
+    dev: Deno.env.get("ENV") === "development",
+  })
+);
 
 // 404 Handler
-site.setNotFoundHandler(async (c) => c.html(await NotFoundLayout()));
+site.notFound(async (c) => c.html(await NotFoundLayout()));
 
 Deno.serve({
   port: 8080,
